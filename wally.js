3ff3fd4ec0d84b85cf837d64abbd740959cc9986
yuuichiyo <65098604+yuuichiyo@users.noreply.github.com>
2020-07-21 18:47:38 +0900

    wally upload
    2020/4に作成した後、PCの中で眠っていた「ウォーリーを探せ」をアップロードする。あれから３か月以上も経ったのか。

---
+++ wally.js
@@ -0,0 +1,13 @@
+window.onload=function(){
+  //マウス移動時のイベントをBODYタグに登録する
+  document.body.addEventListener("onmousemove", function(e){
+ 
+    //座標を取得する
+    var mX = e.pageX;  //X座標
+    var mY = e.pageY;  //Y座標
+ 
+    //座標を表示する
+    document.getElementById("txtX").value = mX;
+    document.getElementById("txtY").value = mY;
+  });
+}
